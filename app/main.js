2d584d7ff43db5623a2d93ca1f8c98acd02487b2
estebangccoder <estebangccoder@gmail.com>
2024-05-23 11:12:28 -0500

    Fix: Created the header, the main.js and the index.html are modificated for add the header

--- app/main.js
+++ app/main.js
@@ -1,8 +1,12 @@
 // Import our custom CSS
 import "../src/scss/style.scss";
-//esta función importa el footer
+//This function imports the footer
 import { footer } from "../app/components/Footer/footer";
+// This function imports the header
+import {Header} from "../app/components/Header/Header"
 // Import all of Bootstrap's JS
+
 import * as bootstrap from "bootstrap";
 
+Header()
 footer();
