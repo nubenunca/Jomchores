f72a97b25e588f8fdc94b7e818f8caba3cc96dae
pruiz5514 <pablo_ruiza@hotmail.com>
2024-05-23 07:24:15 -0500

    fix: Se indica que los campos de los formularios son obligatoriod llenarlos

--- app/components/FormWorker/FormWorker.js
+++ app/components/FormWorker/FormWorker.js
@@ -14,60 +14,60 @@ export const FormWorker = () => {
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="name-worker">Nombre</label>
-                    <input name="name-worker" id="name-worker" type="text" class = "input-worker" placeholder = "Rafael" >
+                    <input name="name-worker" id="name-worker" type="text" class = "input-worker" placeholder = "Rafael" required>
                 </div>
 
                 <div class="inputWorker-container">
                     <label for="lastName-worker">Apellido</label>
-                    <input name="lastName-worker" id="lastName-worker" type="text" class = "input-worker" placeholder = "González" >
+                    <input name="lastName-worker" id="lastName-worker" type="text" class = "input-worker" placeholder = "González" required>
                 </div>
             </div>
             
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="email-worker">Correo electrónico</label>
-                    <input name="email-worker" id="email-worker" type="email" class = "input-worker" placeholder = "rafael@jomchores.com" >
+                    <input name="email-worker" id="email-worker" type="email" class = "input-worker" placeholder = "rafael@jomchores.com" required>
                 </div>
 
                 <div class="inputWorker-container">
                     <label for="userName-worker">Nombre de usuario</label>
-                    <input name="userName-worker" id="userName-worker" type="text" class = "input-worker" placeholder = "rafagonza" >
+                    <input name="userName-worker" id="userName-worker" type="text" class = "input-worker" placeholder = "rafagonza" required>
                 </div>
             </div>
 
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="password-worker">Contraseña</label>
-                    <input name="password-worker" id="password-worker" type="password" class = "input-worker" placeholder = "Contraseña" >
+                    <input name="password-worker" id="password-worker" type="password" class = "input-worker" placeholder = "Contraseña" required>
                 </div>
 
                 <div class="inputWorker-container">
                     <label for="verifyPassword-worker">Ingrese nuevamente la contraseña</label>
-                    <input name="verifyPassword-worker" id="verifyPassword-worker" type="password" class = "input-worker" placeholder = "Contraseña" >
+                    <input name="verifyPassword-worker" id="verifyPassword-worker" type="password" class = "input-worker" placeholder = "Contraseña" required>
                 </div>
             </div>
             
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="phone-worker">Celular</label>
-                    <input name="phone-worker" id="phone-worker" type="tel" class = "input-worker" placeholder = "3202146890" >
+                    <input name="phone-worker" id="phone-worker" type="tel" class = "input-worker" placeholder = "3202146890" required>
                 </div>
 
                 <div class="inputWorker-container">
                     <label for="address-worker">Dirección</label>
-                    <input name="address-worker" id="address-worker" type="text" class = "input-worker" placeholder = "cra 22 #33 -120" >
+                    <input name="address-worker" id="address-worker" type="text" class = "input-worker" placeholder = "cra 22 #33 -120" required>
                 </div>
             </div>
             
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="id-worker">Número de cedúla</label>
-                    <input name="id-worker" id="id-worker" type="number" class = "input-worker" placeholder = "1234567890 " >
+                    <input name="id-worker" id="id-worker" type="number" class = "input-worker" placeholder = "1234567890 " required>
                 </div>
                 
                 <div class="inputWorker-container">
                     <label for="neighborhood-worker">Barrio / Municipio</label>
-                    <input list="neighborhoods-worker" name="neighborhood-worker" id="neighborhood-worker" placeholder = "Castilla" >
+                    <input list="neighborhoods-worker" name="neighborhood-worker" id="neighborhood-worker" placeholder = "Castilla" required>
                     
                     <datalist id="neighborhoods-worker">
                         <option data-value="popular">Popular</option>
@@ -107,13 +107,13 @@ export const FormWorker = () => {
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="certifications-worker">Certificaciones (opcional)</label>
-                    <input name="certifications-worker" id="certifications-worker" class = "file-worker" type="file" >
+                    <input name="certifications-worker" id="certifications-worker" class = "file-worker" type="file">
                     <button type="button" class="workerFile-button" onclick = "document.getElementById('certifications-worker').click()">Seleccionar archivo</button>
                 </div>
                 
                 <div class="inputWorker-container">
                     <label for="idDocument-worker">Subir cedúla</label>
-                    <input name="idDocument-worker" id="idDocument-worker" class = "file-worker" type="file" >
+                    <input name="idDocument-worker" id="idDocument-worker" class = "file-worker" type="file" required>
                     <button type="button" class="workerFile-button" onclick = "document.getElementById('idDocument-worker').click()" >Seleccionar archivo</button>
                 </div>
                 
@@ -122,13 +122,13 @@ export const FormWorker = () => {
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="policeRecords-worker">Antecedentes policiales</label>
-                    <input name="policeRecords-worker" id="policeRecords-worker" class = "file-worker" type="file" >
+                    <input name="policeRecords-worker" id="policeRecords-worker" class = "file-worker" type="file" required>
                     <button type="button" class="workerFile-button" onclick = "document.getElementById('policeRecords-worker').click()">Seleccionar archivo</button>
                 </div>
 
                 <div class="inputWorker-container">
                     <label for="profilePicture-worker">Foto de perfil</label>
-                    <input name="profilePicture-worker" id="profilePicture-worker" class = "file-worker" type="file" accept="image/*">
+                    <input name="profilePicture-worker" id="profilePicture-worker" class = "file-worker" type="file" accept="image/*" required>
                     <button type="button" class="workerFile-button" onclick = "document.getElementById('profilePicture-worker').click()">Seleccionar archivo</button>
                 </div>
 
@@ -138,13 +138,13 @@ export const FormWorker = () => {
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="coverPicture-worker">Foto de portada</label>
-                    <input name="coverPicture-worker" id="coverPicture-worker" class = "file-worker" type="file" accept="image/*">
+                    <input name="coverPicture-worker" id="coverPicture-worker" class = "file-worker" type="file" accept="image/*" required>
                     <button type="button" class="workerFile-button" onclick = "document.getElementById('coverPicture-worker').click()">Seleccionar archivo</button>
                 </div>
 
                 <div class="inputWorker-container">
                     <label for="category-worker">Categoría</label>
-                    <select name="category-worker" id="category-worker" class = "input-worker" >
+                    <select name="category-worker" id="category-worker" class = "input-worker" required>
                         <option disabled selected ></option>
                         <option value="plomeria">Plomería</option>
                         <option value="aseo">Aseo</option>
@@ -156,18 +156,18 @@ export const FormWorker = () => {
             
             <div class="inputWorker-container">
                 <label for="description-worker">Descripción del trabajo</label>
-                <textarea name="description-worker" id="description-worker" class = "worker-textarea" placeholder = "Descripción de su trabajo"></textarea>
+                <textarea name="description-worker" id="description-worker" class = "worker-textarea" placeholder = "Descripción de su trabajo" required></textarea>
             </div>
             
             <div class="inputWorker-container">
                 <label for="price-worker">Valor hora/ Valor servicio (Valle de Aburrá)</label>
-                <input name="price-worker" id="price-worker" type="number" class = "input-worker" placeholder = "Valor servicio ($ COP)" >
+                <input name="price-worker" id="price-worker" type="number" class = "input-worker" placeholder = "Valor servicio ($ COP)" required>
             </div>
 
             <div class="inputWorker-container">
                 <div class="workerConditions-container">
                     <label for="conditions-worker">Acepto los terminos y condiciones</label>
-                    <input type="checkbox" name="conditions-worker" id="conditions-worker" >
+                    <input type="checkbox" name="conditions-worker" id="conditions-worker" required>
                 </div>
             </div>
 
@@ -176,7 +176,6 @@ export const FormWorker = () => {
             </div>
         </form>
     `
-
-    
     return section
 }
+
