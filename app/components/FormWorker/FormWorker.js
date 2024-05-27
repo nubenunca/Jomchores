945210ed44b4f3ab66df08488462e9653d01816e
pruiz5514 <pablo_ruiza@hotmail.com>
2024-05-26 22:46:49 -0500

    feat: functios to add workers is added

--- app/components/FormWorker/FormWorker.js
+++ app/components/FormWorker/FormWorker.js
@@ -29,11 +29,13 @@ export const FormWorker = () => {
                 <div class="inputWorker-container">
                     <label for="email-worker">Correo electrónico</label>
                     <input name="email-worker" id="email-worker" type="email" class = "input-worker" placeholder = "rafael@jomchores.com" required>
+                    <span class="email-security"></span>
                 </div>
 
                 <div class="inputWorker-container">
                     <label for="userName-worker">Nombre de usuario</label>
                     <input name="userName-worker" id="userName-worker" type="text" class = "input-worker" placeholder = "rafagonza" required>
+                    <span class="username-security"></span>
                 </div>
             </div>
 
@@ -41,11 +43,13 @@ export const FormWorker = () => {
                 <div class="inputWorker-container">
                     <label for="password-worker">Contraseña</label>
                     <input name="password-worker" id="password-worker" type="password" class = "input-worker" placeholder = "Contraseña" required>
+                    <span class="password-security"></span>
                 </div>
 
                 <div class="inputWorker-container">
                     <label for="verifyPassword-worker">Ingrese nuevamente la contraseña</label>
                     <input name="verifyPassword-worker" id="verifyPassword-worker" type="password" class = "input-worker" placeholder = "Contraseña" required>
+                    <span class="password-coincidence"></span>
                 </div>
             </div>
             
@@ -53,6 +57,7 @@ export const FormWorker = () => {
                 <div class="inputWorker-container">
                     <label for="phone-worker">Celular</label>
                     <input name="phone-worker" id="phone-worker" type="tel" class = "input-worker" placeholder = "3202146890" required>
+                    <span class="phone-security"></span>
                 </div>
 
                 <div class="inputWorker-container">
@@ -64,7 +69,8 @@ export const FormWorker = () => {
             <div class = "pcView-container">
                 <div class="inputWorker-container">
                     <label for="id-worker">Número de cedúla</label>
-                    <input name="id-worker" id="id-worker" type="number" class = "input-worker" placeholder = "1234567890 " required>
+                    <input name="id-worker" id="id-worker" type="number" class = "input-worker" placeholder = "123456789" required>
+                    <span class="id-security"></span>
                 </div>
                 
                 <div class="inputWorker-container">
@@ -110,13 +116,13 @@ export const FormWorker = () => {
                 <div class="inputWorker-container">
                     <label for="certifications-worker">Certificaciones (opcional)</label>
                     <input name="certifications-worker" id="certifications-worker" class = "file-worker" type="file">
-                    <button type="button" class="workerFile-button" onclick = "document.getElementById('certifications-worker').click()">Seleccionar archivo</button>
+                    <button type="button" id="certifications-workerButton" class="workerFile-button" onclick = "document.getElementById('certifications-worker').click()">Seleccionar archivo</button>
                 </div>
                 
                 <div class="inputWorker-container">
                     <label for="idDocument-worker">Subir cedúla</label>
                     <input name="idDocument-worker" id="idDocument-worker" class = "file-worker" type="file" required>
-                    <button type="button" class="workerFile-button" onclick = "document.getElementById('idDocument-worker').click()" >Seleccionar archivo</button>
+                    <button type="button" id="idDocument-workerButton" class="workerFile-button" onclick = "document.getElementById('idDocument-worker').click()" >Seleccionar archivo</button>
                 </div>
                 
             </div>
@@ -125,7 +131,7 @@ export const FormWorker = () => {
                 <div class="inputWorker-container">
                     <label for="policeRecords-worker">Antecedentes policiales</label>
                     <input name="policeRecords-worker" id="policeRecords-worker" class = "file-worker" type="file" required>
-                    <button type="button" class="workerFile-button" onclick = "document.getElementById('policeRecords-worker').click()">Seleccionar archivo</button>
+                    <button type="button" id="policeRecords-workerButton" class="workerFile-button" onclick = "document.getElementById('policeRecords-worker').click()">Seleccionar archivo</button>
                 </div>
 
                 <div class="inputWorker-container">
--- app/js/formFunctions.js
+++ app/js/formFunctions.js
@@ -27,10 +27,14 @@ export function validatePasswords (password, confirmPassword){
 // Function that informs if a data is already registered in the database
 async function validateInformationWarning (key,inputInformation, url, span, nameKey){
     inputInformation.addEventListener("change", async()=>{
-        const response = await fetch (`${url}?${key}=${inputInformation.value}`);
+        const response = await fetch (`${url}`);
         const data = await response.json();
 
-        if(data.length === 0){
+        const filterData = data.filter(worker => (
+            worker.key == inputInformation
+        ));
+
+        if(filterData.length === 0){
             span.textContent = ``
             return true
         }else{
@@ -42,10 +46,14 @@ async function validateInformationWarning (key,inputInformation, url, span, name
 
 // Function that validates that the information is not in the database.
 async function validateInformation(key,inputInformation, url){
-    const response = await fetch (`${url}?${key}=${inputInformation.value}`);
-    const data = await response.json();
+    const response = await fetch (url);
+        const data = await response.json();
 
-    if(data.length === 0){
+        const filterData = data.filter(worker => (
+            worker.key == inputInformation
+        ));
+
+    if(filterData.length === 0){
         return true
     }else{
 
@@ -63,8 +71,142 @@ export function selectedFile (file, button){
     })
 }
 
+//Function that informs if the password is at least 8 characters long, has at least one number, 1 capital letter and 1 special character
+export function passwordSecurityWarning (password, span){
+    password.addEventListener("input",()=>{
+        const capitalLetter = /[A-Z]/.test(password.value);
+        const number = /\d/.test(password.value);
+        const specialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password.value);
+        const passwordLength = password.value.length >= 8;
+
+        if (capitalLetter && number && specialCharacter && passwordLength){
+            span.textContent = ""
+
+            return true
+        }
+        else{
+            span.textContent = "La contraseña debe contener al menos un numero, una mayuscula, un caracter especial y tener almnenos 8 caracteres";
+            return false
+        }
+    })
+}
+
+//Function that validates that the password is secure
+export function passwordSecurity (password){
+    const capitalLetter = /[A-Z]/.test(password.value);
+    const number = /\d/.test(password.value);
+    const specialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password.value);
+    const passwordLength = password.value.length >= 8;
+
+    if (capitalLetter && number && specialCharacter && passwordLength){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
 //Password encryption function
 export function encPass(password){
     let encrypted = CryptoJS.AES.encrypt(password.value,password.value).toString();
     return encrypted;
+}
+
+// Function to add the worker to the data base.
+export async function createWorker (name, lastName, email, username, password, phone, address, neighborhood, id, category, workDescription, cost,img, url){
+
+    const newWorker = {
+        name: name.value,
+        lastName: lastName.value,
+        email: email.value,
+        username: username.value,
+        password: password,
+        phone: phone.value,
+        address: address.value,
+        neighborhood: neighborhood.value,
+        id: id.value,
+        category: category.value,
+        workDescription: workDescription.value,
+        cost: cost.value,
+        img: img.value
+    } 
+
+    console.log(newWorker);
+
+    await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newWorker)
+    })
+}
+
+export function addWorker(){
+    const form = document.querySelector(".worker-form");
+    const nameWorker = document.querySelector("#name-worker");
+    const lastNameWorker = document.querySelector("#lastName-worker");
+    const emailWorker = document.querySelector("#email-worker");
+    const emailWorkerSpan = document.querySelector(".email-security");
+    const usernameWorker = document.querySelector("#userName-worker");
+    const usernameWorkerSpan = document.querySelector(".username-security")
+    const passwordWorker = document.querySelector("#password-worker");
+    const passwordSpan = document.querySelector(".password-security")
+    const verifyPasswordWorker = document.querySelector("#verifyPassword-worker");
+    const passwordCoincidence = document.querySelector(".password-coincidence")
+    const phoneWorker = document.querySelector("#phone-worker");
+    const phoneWorkerSpan = document.querySelector(".phone-security")
+    const addressWorker = document.querySelector("#address-worker");
+    const neighborhoodWorker = document.querySelector("#neighborhood-worker");
+    const idWorker = document.querySelector("#id-worker");
+    const idWorkerSpan = document.querySelector(".id-security")
+    const certificationWorker = document.querySelector("#certifications-worker")
+    const certificationWorkerButton = document.querySelector("#certifications-workerButton")
+    const idDocumentWorker = document.querySelector("#idDocument-worker")
+    const idDocumentWorkerButton = document.querySelector("#idDocument-workerButton")
+    const profilePictureWorker = document.querySelector("#profilePicture-worker")
+    const profilePictureWorkerButton = document.querySelector("#profilePicture-workerButton")
+    const policeRecordsWorker = document.querySelector("#policeRecords-worker")
+    const policeRecordsWorkerButton = document.querySelector("#policeRecords-workerButton")
+    const categoryWorker = document.querySelector("#category-worker");
+    const descriptionWorker = document.querySelector("#description-worker");
+    const priceWorker = document.querySelector("#price-worker");
+
+    const url = "https://55nafuq2d0.execute-api.us-east-2.amazonaws.com/desarrollo/workers"
+    
+    selectedFile (certificationWorker, certificationWorkerButton);
+    selectedFile (idDocumentWorker, idDocumentWorkerButton);
+    selectedFile (policeRecordsWorker, policeRecordsWorkerButton);
+    selectedFile (profilePictureWorker, profilePictureWorkerButton);
+
+    validatePasswordsWarning(passwordWorker,verifyPasswordWorker,passwordCoincidence);
+    passwordSecurityWarning (passwordWorker, passwordSpan);
+    validateInformationWarning ("email", emailWorker, url,emailWorkerSpan, "correo electronico")
+    validateInformation ("username", usernameWorker, url,usernameWorkerSpan, "nombre de usuario")
+    validateInformation ("phone", phoneWorker, url,phoneWorkerSpan, "número de celular")
+    validateInformation ("id", idWorker, url,idWorkerSpan, "El numero de cedula")
+
+    form.addEventListener("submit", async(event)=>{
+        event.preventDefault();
+        const checkPassword = validatePasswords (passwordWorker,verifyPasswordWorker);
+        const checkPasswordSecurity = passwordSecurity (passwordWorker);
+        const checkEmail = await validateInformation ("email", emailWorker, url)
+        const checkUsername = await validateInformation ("username", usernameWorker, url)
+        const checkephoneUsername = await validateInformation ("phone", phoneWorker, url)
+        const checkIdUsername = await validateInformation ("id", idWorker, url)
+
+        console.log(checkPassword,checkPasswordSecurity, checkEmail, checkUsername, checkephoneUsername, checkIdUsername);
+        const encryptedPassword = encPass(passwordWorker)
+        createWorker (nameWorker, lastNameWorker, emailWorker, usernameWorker, encryptedPassword, phoneWorker, addressWorker, neighborhoodWorker, idWorker, categoryWorker, descriptionWorker, priceWorker,profilePictureWorker, url)
+        // if(checkPassword && checkPasswordSecurity && checkEmail && checkUsername && checkephoneUsername && checkIdUsername){
+        //     const encryptedPassword = encPass(passwordWorker)
+        //     createWorker (nameWorker, lastNameWorker, emailWorker, usernameWorker, encryptedPassword, phoneWorker, addressWorker, neighborhoodWorker, idWorker, categoryWorker, descriptionWorker, priceWorker,profilePictureWorker, url)
+        // }else{
+        //     alert("No se pudo crear")
+        // }
+        
+
+    })
+    
+    
 }
