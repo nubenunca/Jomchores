2a53e21dc0069f257916c485b69911780bfcd54a
pruiz5514 <pablo_ruiza@hotmail.com>
2024-05-28 11:02:09 -0500

    feat: modals are added in worker and user form

--- app/components/FormWorker/FormWorker.js
+++ app/components/FormWorker/FormWorker.js
@@ -166,11 +166,7 @@ export const FormWorker = () => {
 
             <div class="inputWorker-container">
                 <div class="workerConditions-container">
-                    <label for="conditions-worker">Acepto los terminos y condiciones</label>
-                    
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Launch demo modal
-                    </button>
+                    <label for="conditions-worker">Acepto los <a href="" data-bs-toggle="modal" data-bs-target="#exampleModal"> terminos y condiciones </a> </label>                
                     <input type="checkbox" name="conditions-worker" id="conditions-worker" required>
                 </div>
             </div>
@@ -180,22 +176,19 @@ export const FormWorker = () => {
             </div>
 
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Terminos y condiciones</h1>
+                    </div>
+                    <div class="modal-body">
+                        <p>Jomchores</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    </div>
+                    </div>
                 </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-                </div>
-            </div>
             </div>
         </form>
     `
