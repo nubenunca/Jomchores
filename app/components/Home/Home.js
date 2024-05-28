c24a1885890e36f2ee55762d32d29688ecc21057
luisruro <lerubioromero@gmail.com>
2024-05-28 09:03:31 -0500

    Se gregan animaciones para el carrusel y la sección de la gráfica de los servicios más demandados

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -57,7 +57,7 @@ const sectionTwo = main.appendChild(createSectionTwo());
 
 function createCarousel() {
     sectionTwo.innerHTML = `
-        <div id="carouselExampleInterval" class="carousel slide mt-3" data-bs-ride="carousel">
+        <div data-aos="fade-up-right" id="carouselExampleInterval" class="carousel slide mt-3" data-bs-ride="carousel">
             <div class="carousel-inner";>
             <div class="carousel-item active" data-bs-interval="10000">
                 <img src="./public/img/img1.webp" class="d-block w-100" alt="image carousel">
@@ -133,6 +133,7 @@ createComments();
 function createSectionFour() {
     const sectionFour = document.createElement('section');
     sectionFour.classList.add("container-fluid");
+    sectionFour.setAttribute("data-aos", "flip-left", "data-aos-once", "false");
     sectionFour.innerHTML = "";
     return sectionFour;
 }
