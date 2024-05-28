0857454dbbfac82976b810ea0f14ee19fe12af90
luisruro <lerubioromero@gmail.com>
2024-05-28 07:55:59 -0500

    Se agrega animación a los botones del CTA de las cards

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -36,7 +36,7 @@ async function createCardService() {
             <div class="card-body">
                 <h5 class="card-title text-center" style="height:48px;">${service.title}</h5>
                 <p class="card-text text-center" style="height:96px;">${service.description}</p>
-                <a href="#" class="btn btn-red text-white fw-medium d-flex align-content-center justify-content-center">${service.callToAction}</a>
+                <a href="#" class="btn btn-red text-white fw-medium d-flex align-content-center justify-content-center animate__animated animate__pulse animate__slower animate__infinite">${service.callToAction}</a>
             </div>
         </div>
         `;
